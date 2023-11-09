0439f2f01ab7586916270fda6c5278a9df269522
ellen <ellen_yu@pixnet.tw>
2023-11-09 18:21:38 +0800

    feat: 初步加入useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+        const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+    })
+  }, [])
+  
 
   function handleStartRemovePlace(id) {
     modal.current.open();
